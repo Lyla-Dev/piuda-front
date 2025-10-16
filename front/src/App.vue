0e46c95d5bf904c4ff8fe38a29338d39b71ba699
Minseo <rlaalstj0510@naver.com>
2025-10-16 12:03:11 +0900

    eature/chore/setting/fix/refactor : 작성자, 활동인원 수, 활동단체 입력칸

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="padding: 40px; max-width: 600px; margin: auto;">
+    <h2>InputField 테스트</h2>
+
+    <InputField
+      label="작성자"
+      required
+      v-model="form.writer"
+      placeholder="홍길동"
+    />
+
+    <InputField
+      label="활동인원 수"
+      required
+      type="number"
+      v-model="form.count"
+      placeholder="0명"
+    />
+
+    <InputField
+      label="활동단체"
+      required
+      v-model="form.title"
+      placeholder="예: 제주바당"
+    />
+
+    <p style="margin-top: 20px;">입력 내용 확인:</p>
+    <pre>{{ form }}</pre>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue"
+import InputField from "@/components/common/InputField.vue"
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const form = ref({
+  writer: "",
+  count: "",
+  groupname: ""
+})
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
