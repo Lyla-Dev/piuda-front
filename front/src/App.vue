f65347358540a96efa8b7da39f8e80f3c7c208de
Minkyeong-Choi <minkyeong.choi@outlook.com>
2025-11-08 02:46:43 +0900

    chore : barbel error

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <div id="app">
-    <!-- ActivityFormView 컴포넌트를 렌더링하여 ESLint 에러를 해결합니다. -->
     <ActivityFormView /> 
   </div>
 </template>
 
 <script>
-// ActivityFormView 컴포넌트가 pages나 components 폴더에 있을 경우의 임시 경로입니다.
-// 실제 프로젝트 구조에 따라 이 경로는 다를 수 있지만, 이전에 언급된 경로를 가정합니다.
 import ActivityFormView from './components/ActivityView/ActivityFormView.vue'; 
 
 export default {
